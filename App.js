76dcab5a4eddaaa9cc855009c00f315eb3caf154
Leonardo Augusto <Leonardo1998.augusto@gmail.com>
2021-06-29 23:38:53 -0300

    commit 1
    Commit numero 1

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+
+export default function App() {
+
+   const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Sobre' component={Sobre}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+
+
+export default function Home({ navigation }){
+    return(
+        <View>
+            <Text>Página Home</Text>
+            
+        </View>
+    );
+}
